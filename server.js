e9db1b4cf91a4c24628683f6ba5e52b6c5d2b505
walter090 <walter.wu090@gmail.com>
2018-08-21 19:06:00 -0400

    Update header

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Origin", "Content-Type, Authorization, Accept, X-Requested-With");
     res.header("Access-Control-Allow-Origin", "OPTIONS, TRACE, GET, HEAD, POST, PUT");
-    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Credentials", "true");
     next();
 });
 
