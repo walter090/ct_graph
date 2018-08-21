63609cef643bcfab60cf50ea960b5d109cbccc9e
walter090 <walter.wu090@gmail.com>
2018-08-21 18:47:01 -0400

    Update header

--- server.js
+++ server.js
@@ -125,7 +125,6 @@ const app = express();
 app.use(loggingMiddleware);
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
