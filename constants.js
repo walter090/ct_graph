41c8d0caf5e30a577ee0981992df4477cc0863e3
walter090 <walter.wu090@gmail.com>
2018-08-21 02:55:38 -0400

    Update constants

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const API = Object.freeze({
-    'CUSTOMER_API_ROOT': 'http://customer-pre.us-west-2.elasticbeanstalk.com/customers/',
-    'TRANSACTION_API_ROOT': 'http://operation.us-west-2.elasticbeanstalk.com/transactions/',
+    'CUSTOMER_API_ROOT': 'http://customer-pre.us-west-2.elasticbeanstalk.com/',
+    'TRANSACTION_API_ROOT': 'http://operation.us-west-2.elasticbeanstalk.com/',
 });
 
 module.exports = API;
