4727671f5cf9ae513c80ee1ca2af5d6c06a3b4a8
walter090 <walter.wu090@gmail.com>
2018-08-21 01:45:05 -0400

    Allow constants to repo

---
+++ constants.js
@@ -0,0 +1,6 @@
+const API = Object.freeze({
+    'CUSTOMER_API_ROOT': 'http://customer-pre.us-west-2.elasticbeanstalk.com/customers/',
+    'TRANSACTION_API_ROOT': 'http://operation.us-west-2.elasticbeanstalk.com/transactions/',
+});
+
+module.exports = API;
